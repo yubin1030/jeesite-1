d51b5733b91f77a93a4be2edd350ca9ec2000966
yubin <byu@xiaohongshu.com>
2017-03-13 11:23:19 +0800

    02

---
+++ src/main/java/com/thinkgem/jeesite/modules/sys/utils/HelloWorld.java
@@ -0,0 +1,18 @@
+package com.thinkgem.jeesite.modules.sys.utils;
+
+import com.thinkgem.jeesite.common.utils.DateUtils;
+
+/**
+ * Created by yubin on 17/3/12.
+ */
+public class HelloWorld {
+
+    public static void main(String args[]){
+        long a=System.currentTimeMillis();
+        DateUtils.formatDateTime(a);
+    }
+}
+/**
+ * @author
+ * @create 2017-03-12 上午9:20
+ **/
